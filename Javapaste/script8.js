1498faa959d8033e916ea38d5cbd982f8541d967
joao90108-source <joao90108@edu.unipar.br>
2026-05-25 20:47:33 -0300

    Add files via upload

--- Javapaste/script8.js
+++ Javapaste/script8.js
@@ -10,10 +10,14 @@ let h3NomesOrdenados = document.querySelector ("#h3NomesOrdenados");
 function verificarIdades(){
 
    //retirando o ano digitado e convertendo para número
-   let ano1 = number(inputAno1.value);
-   let ano2 = number(inputAno2.value);
-   let ano3 = number(inputAno3.value);
-
+   let ano1 = Number(inputAno1.value);
+   let ano2 = Number(inputAno2.value);
+   let ano3 = Number(inputAno3.value);
+ 
+   let nome1 = inputNome1.value;
+   let nome2 = inputNome2.value;
+   let nome3 = inputNome3.value;
+   
    let anoAtual = 2026;
 
    //Calculando a idade de cada um
@@ -21,7 +25,48 @@ function verificarIdades(){
    let idade2 = anoAtual - ano2;
    let idade3 = anoAtual - ano3;
 
-}
+   //Variaveis para armazenar os resultados obtidos
+   let maisVelha="";
+   let segunda="";
+   let maisNova=""; 
+   
+   //Verificar se o nome1 é o mais velho
+   if(idade1 >= idade2 && idade1>= idade3){
+      maisVelha = nome1 +"-"+idade1+"anos"
+        //Verificar quem é o mais velho entr 2 e 3   
+        if(idade2 >= idade3){
+            segunda = nome2 +"-"+ idade2+" anos";
+            maisNova = nome3 +"-"+idade3+ "anos";   
+        }else{
+            segunda = nome3+"-"+idade3+" anos";
+            maisNova = nome2+"-"+idade2+" anos";
+        }     
+    }else if(idade2 >= idade1 && idade2>= idade3){
+      maisVelha = nome2 +"-"+idade2+"anos"
+      
+        if(idade1 >= idade3){
+            segunda = nome1 +"-"+ idade1+" anos";
+            maisNova = nome3 +"-"+idade3+ "anos";   
+        }else{
+            segunda = nome3+"-"+idade3+" anos";
+            maisNova = nome1+"-"+idade1+" anos";
+        }     
+    }else{
+        maisVelha = nome3 +"-"+idade3+"anos"
+        if(idade1 >= idade2){
+            segunda = nome1 +"-"+idade1+" anos";
+            maisNova = nome2+"-"+idade2+" anos";
+        }else{
+            segunda = nome2+"-"+idade2+" anos";
+            maisNova = nome1+"-"+idade1+" anos";
+        }
+    }    
+   
+    h3NomesOrdenados.innerHTML= "Pessoa mais velha: "+maisVelha+"<br>"+
+                                "2 pessoa mais velha: "+segunda+"<br>"+
+                                "Pessoa mais nova: "+maisNova            
+} 
+     
 
 btOrdenar.onclick = function(){
     verificarIdades();
